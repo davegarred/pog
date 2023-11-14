0daddd175e38e86e3e12749c8ca68cec21912560
Dave Garred <dave.garred@gmail.com>
2023-11-14 07:32:26 -0800

    Correct discord User dto

--- discord_api/src/interaction_request/user.rs
+++ discord_api/src/interaction_request/user.rs
@@ -5,7 +5,33 @@ use serde::{Deserialize, Serialize};
 pub struct User {
     pub id: String,
     pub username: String,
+    pub discriminator: String,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub global_name: Option<String>,
-    pub avatar: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub avatar: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub bot: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub system: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub mv_enabled: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub banner: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub accent_color: Option<u32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub locale: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub verified: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub email: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub flags: Option<u32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub premium_type: Option<u32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub public_flags: Option<u32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub avatar_decoration: Option<String>,
 }
