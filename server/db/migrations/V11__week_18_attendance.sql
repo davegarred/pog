26d8bb4e32501076f4f0969d8f1cabceef703a56
Dave Garred <dave.garred@gmail.com>
2024-01-08 07:06:54 -0800

    week 18 attendance

---
+++ server/db/migrations/V11__week_18_attendance.sql
@@ -0,0 +1,10 @@
+INSERT INTO ff_attendance(attendance_id, owner, week, date)
+VALUES (nextval('seq_ff_attendance_id'), 695398918694895710, 18, '2023-01-07'),
+
+       (nextval('seq_ff_attendance_id'), 1048049562960539648, 18, '2023-01-08'),
+       (nextval('seq_ff_attendance_id'), 431634941626023936, 18, '2023-01-08'),
+       (nextval('seq_ff_attendance_id'), 695398918694895710, 18, '2023-01-08'),
+
+       (nextval('seq_ff_attendance_id'), 1048049562960539648, 18, '2023-01-08'),
+       (nextval('seq_ff_attendance_id'), 431634941626023936, 18, '2023-01-08'),
+       (nextval('seq_ff_attendance_id'), 695398918694895710, 18, '2023-01-08');
