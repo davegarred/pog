5f59b496335789fd9ece2e10ea6279c2a508d1e8
Dave Garred <dave.garred@gmail.com>
2024-09-24 14:49:47 -0700

    Week 3 attendance

---
+++ server/db/migrations/V16__2024_week_3_attendance.sql
@@ -0,0 +1,29 @@
+INSERT INTO ff_attendance(attendance_id, owner, week, date, season)
+VALUES (nextval('seq_ff_attendance_id'), 695398918694895710, 3, '2024-09-19', 2024),
+       (nextval('seq_ff_attendance_id'), 1046484657249718414, 3, '2024-09-19', 2024),
+       (nextval('seq_ff_attendance_id'), 1045764168210448384, 3, '2024-09-19', 2024),
+       (nextval('seq_ff_attendance_id'), 689977564202401792, 3, '2024-09-19', 2024),
+
+
+       (nextval('seq_ff_attendance_id'), 1054147659289600060, 3, '2024-09-22', 2024),
+       (nextval('seq_ff_attendance_id'), 1045764168210448384, 3, '2024-09-22', 2024),
+       (nextval('seq_ff_attendance_id'), 156425668270358529, 3, '2024-09-22', 2024),
+       (nextval('seq_ff_attendance_id'), 689977564202401792, 3, '2024-09-22', 2024),
+       (nextval('seq_ff_attendance_id'), 1045795671489380354, 3, '2024-09-22', 2024),
+       (nextval('seq_ff_attendance_id'), 1048049562960539648, 3, '2024-09-22', 2024),
+
+       (nextval('seq_ff_attendance_id'), 1046484657249718414, 3, '2024-09-22', 2024),
+       (nextval('seq_ff_attendance_id'), 896920111523459082, 3, '2024-09-22', 2024),
+       (nextval('seq_ff_attendance_id'), 1045764168210448384, 3, '2024-09-22', 2024),
+       (nextval('seq_ff_attendance_id'), 431634941626023936, 3, '2024-09-22', 2024),
+       (nextval('seq_ff_attendance_id'), 1045795671489380354, 3, '2024-09-22', 2024),
+       (nextval('seq_ff_attendance_id'), 1048049562960539648, 3, '2024-09-22', 2024),
+
+
+       (nextval('seq_ff_attendance_id'), 1046484657249718414, 3, '2024-09-23', 2024),
+       (nextval('seq_ff_attendance_id'), 1045764168210448384, 3, '2024-09-23', 2024),
+       (nextval('seq_ff_attendance_id'), 431634941626023936, 3, '2024-09-23', 2024),
+       (nextval('seq_ff_attendance_id'), 1045795671489380354, 3, '2024-09-23', 2024),
+       (nextval('seq_ff_attendance_id'), 695398918694895710, 3, '2024-09-23', 2024);
+
+
