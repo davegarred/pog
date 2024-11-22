cbfc2bb0e40c3504cd36a13fd105a0a447dc814c
Dave Garred <dave.garred@gmail.com>
2024-11-21 16:33:22 -0800

    Week 9-11 attendance

---
+++ server/db/migrations/V22__2024_week_9_11_attendance.sql
@@ -0,0 +1,32 @@
+INSERT INTO ff_attendance(attendance_id, owner, week, date, season)
+VALUES (nextval('seq_ff_attendance_id'), 695398918694895710, 9, '2024-10-31', 2024),
+       (nextval('seq_ff_attendance_id'), 1050119194533961860, 9, '2024-10-31', 2024),
+
+       (nextval('seq_ff_attendance_id'), 1048049562960539648, 9, '2024-11-03', 2024),
+
+       (nextval('seq_ff_attendance_id'), 1048049562960539648, 9, '2024-11-03', 2024),
+       (nextval('seq_ff_attendance_id'), 431634941626023936, 9, '2024-11-03', 2024),
+       (nextval('seq_ff_attendance_id'), 1046484657249718414, 9, '2024-11-03', 2024),
+
+
+       (nextval('seq_ff_attendance_id'), 156425668270358529, 10, '2024-11-10', 2024),
+       (nextval('seq_ff_attendance_id'), 1050119194533961860, 10, '2024-11-10', 2024),
+       (nextval('seq_ff_attendance_id'), 431634941626023936, 10, '2024-11-10', 2024),
+
+       (nextval('seq_ff_attendance_id'), 156425668270358529, 10, '2024-11-10', 2024),
+       (nextval('seq_ff_attendance_id'), 1050119194533961860, 10, '2024-11-10', 2024),
+       (nextval('seq_ff_attendance_id'), 431634941626023936, 10, '2024-11-10', 2024),
+
+
+       (nextval('seq_ff_attendance_id'), 1054147659289600060, 11, '2024-11-17', 2024),
+       (nextval('seq_ff_attendance_id'), 156425668270358529, 11, '2024-11-17', 2024),
+       (nextval('seq_ff_attendance_id'), 431634941626023936, 11, '2024-11-17', 2024),
+       (nextval('seq_ff_attendance_id'), 1048049562960539648, 11, '2024-11-17', 2024),
+
+       (nextval('seq_ff_attendance_id'), 1054147659289600060, 11, '2024-11-17', 2024),
+       (nextval('seq_ff_attendance_id'), 156425668270358529, 11, '2024-11-17', 2024),
+       (nextval('seq_ff_attendance_id'), 431634941626023936, 11, '2024-11-17', 2024),
+       (nextval('seq_ff_attendance_id'), 1048049562960539648, 11, '2024-11-17', 2024);
+
+
+
