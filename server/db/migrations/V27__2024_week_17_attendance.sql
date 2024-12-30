0983c60ec063e588338525529d6e47b72fe7dc23
Dave Garred <dave.garred@gmail.com>
2024-12-30 15:14:48 -0800

    Week 17 attendance

---
+++ server/db/migrations/V27__2024_week_17_attendance.sql
@@ -0,0 +1,23 @@
+INSERT INTO ff_attendance(attendance_id, owner, week, date, season)
+VALUES (nextval('seq_ff_attendance_id'), 896920111523459082, 17, '2024-12-25', 2024),
+
+
+       (nextval('seq_ff_attendance_id'), 695398918694895710, 17, '2024-12-26', 2024),
+       (nextval('seq_ff_attendance_id'), 896920111523459082, 17, '2024-12-26', 2024),
+
+
+       (nextval('seq_ff_attendance_id'), 695398918694895710, 17, '2024-12-29', 2024),
+       (nextval('seq_ff_attendance_id'), 1048049562960539648, 17, '2024-12-29', 2024),
+
+       (nextval('seq_ff_attendance_id'), 1048049562960539648, 17, '2024-12-29', 2024),
+       (nextval('seq_ff_attendance_id'), 896920111523459082, 17, '2024-12-29', 2024),
+       (nextval('seq_ff_attendance_id'), 431634941626023936, 17, '2024-12-29', 2024),
+
+
+       (nextval('seq_ff_attendance_id'), 695398918694895710, 17, '2024-12-30', 2024);
+
+
+
+
+
+
